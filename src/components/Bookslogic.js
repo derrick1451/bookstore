ef00fc931d9f9f152453693f2ef0d5e3c58a58ae
Derrick1451 <haylederrick14@gmail.com>
2023-07-01 12:58:21 +0300

    update booklogic and display books

--- src/components/Bookslogic.js
+++ src/components/Bookslogic.js
@@ -1,14 +1,26 @@
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { useEffect } from 'react';
 import FormInput from './FormInput';
 import BookItem from './BookItem';
+import { fetchBooks } from '../redux/books/booksSlice';
 
 function BooksLogic() {
-  const { books } = useSelector((state) => state.books);
+  const { books, isLoading, error } = useSelector((state) => state.books);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchBooks());
+  }, [dispatch]);
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+  if (error) {
+    return <p>Something went wrong...</p>;
+  }
 
   return (
     <div className="container">
       <ul>
-        {books.map((book) => <BookItem itemProp={book} key={book.id} />)}
+        {books.map((book) => <BookItem itemProp={book} key={book.item_id} />)}
       </ul>
       <FormInput />
     </div>
